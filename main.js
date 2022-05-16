bd43d16e27b20c7f7ff3deeac3ff63f38639aeea
shauryashetty <95406465+shauryashetty@users.noreply.github.com>
2022-05-16 12:14:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL("BirthdayImage.jpg",function(Img)
+        block_image_object=Img;
+
+
+    }
+	
+}
+
+function playSound(){
+	x.play();
+}
